03c003ab13cc467a8f30b8d160f776b11e072b6a
LuisEQ <luiseduardoql@hotmail.com>
2022-03-02 16:30:00 -0700

    FilterFinalwoCSS

---
+++ src/components/Student/StudentFilter.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const StudentFilter = (props) => {
+    const dropdownChangeHandler = (event) => {
+        props.onChangeFilter(event.target.value);
+    };
+
+    return (
+        <div>
+            <div>
+                <label>Filtrar por carrera</label>
+                <select value={props.selected} onChange={dropdownChangeHandler}>
+                    <option value="Engineer">Ingeniería</option>
+                    <option value="Medicina">Medicina</option>
+                    <option value="Programacion">Programación</option>
+                    <option value="Psicologia">Psicología</option>
+                </select>
+            </div>
+        </div>
+    );
+};
+
+export default StudentFilter;
--- src/components/Student/Students.js
+++ src/components/Student/Students.js
@@ -1,40 +1,37 @@
 import React, { useState } from "react";
 import StudentO from "./StudentO";
 import Card from "../Card/Card";
+import StudentFilter from "./StudentFilter";
 import "./Students.css";
 
+
 const Students = (props) => {
+  const [filteredMajor, setFilteredMajor] = useState("Engineer");
+
+  const filterChangeHandler = (selectedMajor) => {
+    setFilteredMajor(selectedMajor);
+  };
+  const filteredStudents = props.items.filter((student) => {
+    return student.major === filteredMajor;
+  });
+
   return (
     <div>
-      <Card className="Students">
-        <StudentO
-          key={props.items[0].id}
-          nameStudent={props.items[0].nameStudent}
-          pfp={props.items[0].pfp}
-          age={props.items[0].age}
-          major={props.items[0].major}
+      <Card className="students">
+        <StudentFilter
+          selected={filteredMajor}
+          onChangeFilter={filterChangeHandler}
         />
-        <StudentO
-          key={props.items[1].id}
-          nameStudent={props.items[1].nameStudent}
-          pfp={props.items[1].pfp}
-          age={props.items[1].age}
-          major={props.items[1].major}
-        />
-        <StudentO
-          key={props.items[2].id}
-          nameStudent={props.items[2].nameStudent}
-          pfp={props.items[2].pfp}
-          age={props.items[2].age}
-          major={props.items[2].major}
-        />
-        <StudentO
-          key={props.items[3].id}
-          nameStudent={props.items[3].nameStudent}
-          pfp={props.items[3].pfp}
-          age={props.items[3].age}
-          major={props.items[3].major}
-        />
+        {filteredStudents.map((student) => (
+          <StudentO
+            key={student.id}
+            nameStudent={student.nameStudent}
+            pfp={student.pfp}
+            age={student.age}
+            major={student.major}
+          />
+        ))}
+
       </Card>
     </div>
   );
