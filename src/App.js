63d1166d3e4044668a99620efac5cf7f68393efa
LuisEQ <luiseduardoql@hotmail.com>
2022-03-02 17:57:08 -0700

    NewChangesToApp

--- src/App.js
+++ src/App.js
@@ -9,46 +9,52 @@ import photo2 from "./components/images/2.png"
 import photo3 from "./components/images/3.png"
 import photo4 from "./components/images/4.png"
 
+const studentsDummy = [
+  {
+    id: "s1",
+    nameStudent: "Julio Cesar",
+    pfp: photo1,
+    major: "Engineer",
+    age: 20,
+  },
+  {
+    id: "s2",
+    nameStudent: "Emiliano Mateo",
+    pfp: photo2,
+    major: "Medicina",
+    age: 20,
+  },
+  {
+    id: "s3",
+    nameStudent: "Daniel Chavez",
+    pfp: photo3,
+    major: "Engineer",
+    age: 20,
+  },
+  {
+    id: "s4",
+    nameStudent: "Eduardo Torres",
+    pfp: photo4,
+    major: "Engineer",
+    age: 20,
+  },
+];
+
 const App = () => {
-  const students = [
-    {
-      id: "s1",
-      nameStudent: "Julio Cesar",
-      pfp: photo1,
-      major: "Engineer",
-      age: 20,
-    },
-    {
-      id: "s2",
-      nameStudent: "Emiliano Mateo",
-      pfp: photo2,
-      major: "Engineer",
-      age: 20,
-    },
-    {
-      id: "s3",
-      nameStudent: "Daniel Chavez",
-      pfp: photo3,
-      major: "Engineer",
-      age: 20,
-    },
-    {
-      id: "s4",
-      nameStudent: "Eduardo Torres",
-      pfp: photo4,
-      major: "Engineer",
-      age: 20,
-    },
-  ];
+
+  const [student, setStudents] = useState(studentsDummy);
+
   const addStudentHandler = (student) => {
-    console.log("In App.js");
-    console.log(student);
+    setStudents((prevStudents) => {
+      return [student, ...prevStudents];
+    });
   };
+
 
   return (
     <div>
       <NewStudent onAddStudent={addStudentHandler} />
-      <Students items={students} />
+      <Students items={student} />
     </div>
   );
 };
